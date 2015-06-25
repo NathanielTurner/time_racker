de7b59654c4f4df12130bd75ff4a3db1a3d8a81c
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-25 17:30:08 -0400

    Create and populate time entry index view which will render a table and list all time entry durations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-get 'time_entries/new'
-get 'time_entries/index'
-get 'time_entries/create'
+  
+  get 'time_entries/new'
+  get 'time_entries/index'
+  get 'time_entries/create'
+
 end
