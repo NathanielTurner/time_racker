04a57448cfe8254277655755dca11e0459dbf031
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 14:54:03 -0400

    implement capybara feature tests on time_entries.

--- test/features/can_create_time_entries_test.rb
+++ test/features/can_create_time_entries_test.rb
@@ -1,15 +1,23 @@
 require "test_helper"
 
 class CanCreateTimeEntriesTest < Capybara::Rails::TestCase
-  test "new form renders" do
+  test "new and index pages render" do
     visit time_entries_new_path
     assert_content page, 'Record a Time Entry'
+    visit time_entries_path
+    assert_content page, 'All Time Entries'
   end
 
   test "form has fields for duration and date" do
     visit time_entries_new_path
     assert page.find('input[type="number"][name*="duration"]')
-  #  assert page.find('input[type="date"]')
     assert page.find('input[type="date"][name*="date"]')
+  end
+
+  test "index page list time entries and thier durations and dates" do
+    visit time_entries_path
+    assert page.has_css?('tr', :count => 3)
+    assert page.has_css?('#duration', :count => 2)
+    assert page.has_css?('#date', :count => 2)
   end
 end
