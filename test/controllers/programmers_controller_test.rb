6cb6aa6357fb4a442f0b565cfe9ec7804d796ffc
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-25 21:37:53 -0400

    Build and populate initial controller test for developers testing new, index, and create actions.

---
+++ test/controllers/programmers_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class ProgrammersControllerTest < ActionController::TestCase
+  setup do
+    @programmer_one = programmers(:one)
+    @programmer_two = programmers(:two)
+  end
+
+
+    test "should get new" do
+      get :new
+      assert_response :success
+      assert_not_nil assigns(:programmer)
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:programmers)
+    end
+
+    test "should create programmer" do
+      assert_difference('Programmer.count') do
+        post :create, programmer: { first_name: "Test", last_name: "Me",
+           email: "testme@example.com", password: "password"}
+      end
+
+      assert_not_nil assigns(:programmer)
+      assert_redirected_to programmers_path
+    end
+
+end
