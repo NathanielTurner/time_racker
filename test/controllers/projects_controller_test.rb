15ed0774af77ae1adecc7442d9261b0d8591c22b
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 19:51:50 -0400

    add controller test. tests actions new, create, and index. checks that they utilize correct parameters.

---
+++ test/controllers/projects_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProgrammersControllerTest < ActionController::TestCase
+
+    test "should get new" do
+      get :new
+      assert_response :success
+      assert_not_nil assigns(:project)
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:projects)
+    end
+
+    test "should create project" do
+      assert_difference('Project.count') do
+        post :create, project: { name: "Koans", time_limit: 3000}
+      end
+      assert_not_nil assigns(:project)
+      assert_redirected_to projects_path
+    end
+end
