e3a64c66eab4e3af511fa64b8c04348dd755ffdb
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 20:59:06 -0400

    add a test to check filling out form and submitting it will make a new time entry

--- test/features/can_create_time_entries_test.rb
+++ test/features/can_create_time_entries_test.rb
@@ -11,14 +11,14 @@ class CanCreateTimeEntriesTest < Capybara::Rails::TestCase
   test "form has fields for duration and date" do
     visit time_entries_new_path
     assert page.find('input[type="number"][name="time_entry[duration]"]')
-    assert page.find('input[type="date"][name*="time_entry[date]"]')
+    assert page.find('input[type="date"][name="time_entry[date]"]')
   end
 
   test "index page list time entries and thier durations and dates" do
     visit time_entries_path
-    assert page.has_css?('tr', :count => 3)
-    assert page.has_css?('#duration', :count => 2)
-    assert page.has_css?('#date', :count => 2)
+    assert page.has_css?('tr', count: 3)
+    assert page.has_css?('#duration', count: 2)
+    assert page.has_css?('#date', count:  2)
   end
 
   test "time entries can be created" do
