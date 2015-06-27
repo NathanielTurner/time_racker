ae3ac9ee72897d17864b03dc66871c602e8602ac
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 21:29:50 -0400

    add a controller with new index create action with defs for params and id getting.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,31 @@
+class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def index
+    @projects = Project.all
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      format.html { redirect_to projects_path,
+          notice: {success: 'Project successfuly created'} }
+    else
+      format.html { render action: 'new',
+          notice: {error: 'Something prevented project from being saved'}}
+    end
+  end
+
+  private
+    def set_project
+      @projects = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:projects).permit(:name, :time_limit)
+    end
+
+end
