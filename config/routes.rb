eaac31afaa3e16d8ea7dc0c29005f0ed27725d42
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 22:23:42 -0400

    add routes and recourses for projects

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'programmers/index'
   get 'programmers/create'
   resources :programmers
+  get 'projects/new'
+  get 'projects/index'
+  get 'projects/create'
+  resources :projects
 end
