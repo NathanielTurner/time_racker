45a9647e86208b2b8f499b9719346a63b0fb9560
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-26 21:10:40 -0400

    rails generate a model with fields name:string, time_limit:integer

--- test/models/time_entry_test.rb
+++ test/models/time_entry_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class TimeEntryTest < ActiveSupport::TestCase
-  test "Time_entries have been created from fixtures" do
-    assert time_entries(:one).duration == 1500
-    assert time_entries(:two).duration == 2000
-#   "I want to count total fixtures, how"  
-  end
+
 end
