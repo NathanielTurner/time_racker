a58bee6074aac45f07091ac2afa72fc97408f822
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-28 16:28:19 -0400

    add test to check _form has field inputs for password and email.

--- test/features/programmers_can_log_in_and_out_test.rb
+++ test/features/programmers_can_log_in_and_out_test.rb
@@ -5,4 +5,11 @@ class ProgrammersCanLogInAndOutTest < Capybara::Rails::TestCase
     visit sessions_new_path
     assert_content page, 'Sign In'
   end
+
+  test "form has fields for email and password" do
+    visit time_entries_new_path
+    assert page.find('input[type="text"][name="session[email]"]')
+    assert page.find('input[type="password"][name="session[password]"]')
+  end
+
 end
