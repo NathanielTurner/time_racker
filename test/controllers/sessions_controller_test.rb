f29f819f5724bd4ae6a10ab99940b9087681f878
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-28 16:10:29 -0400

    test that create action sets session[user-id] to programmer_id using a fixture.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,17 +1,37 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+  setup do
+    @programmer_one = programmers(:john)
+    @programmer_two = programmers(:jane)
+  end
 
   test "shoud get new" do
     get :new
     assert_response :success
+    assert_not_nil assigns(:title)
   end
 
-  test "should post create" do
-    post create:
-    assert_response :success
+  
+
+  test "should set session[user_id] to programmer id" do
+    post :create, session: { email: @programmer_one.email,
+        password: @programmer_one.password }
+    assert_equal @programmer_one.id, session[user_id]
   end
 
   test "should delete destroy" do
     delete destroy:
+    assert_response :success
   end
+
+#  test "create should validate programmer"
+#  post create:
+#    before(:each) do
+#      @ attr = { :email => "johndoe@example.com", :password => "secret" }
+#      Programmer.should_receive(:authenticate).
+#           with(@attr[:email], @attr[:password]).
+#           and_return(nil)
+#    end
+#  end
+end
