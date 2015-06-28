c6355299d80025291a9d7e7639338dc75210e9cc
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-28 17:00:03 -0400

    add test to check that logout button appears after users log in and it logs user out.

--- test/features/programmers_can_log_in_and_out_test.rb
+++ test/features/programmers_can_log_in_and_out_test.rb
@@ -7,20 +7,33 @@ class ProgrammersCanLogInAndOutTest < Capybara::Rails::TestCase
   end
 
   test "signin page renders" do
-    visit sessions_new_path
+    visit signin_path
     assert_content page, 'Sign In'
   end
 
   test "form has fields for email and password" do
-    visit time_entries_new_path
+    visit signin_path
     assert page.find('input[type="text"][name="session[email]"]')
     assert page.find('input[type="password"][name="session[password]"]')
   end
 
   test "programmer can fill in form than be logged in" do
-    visit sessions_new_path
+    visit signin_path
     fill_in('Email', with: @programmer_one.email)
     fill_in('Password', with: @programmer_one.password)
     click_button('Create session')
+    assert_equal @programmer_one.id, session[user_id]
+    assert_redirected_to projects_path
+    visit signout_path
+  end
+
+  test "after login, button shows that logs user out universally" do
+    visit signin_path
+    fill_in('Email', with: @programmer_two.email)
+    fill_in('Password', with: @programmer_two.password)
+    click_button('Create session')
+    assert_equal @programmer_two.id, session[user_id]
+    click_button('Log Out')
+    assert_redirected_to signin_path
   end
 end
