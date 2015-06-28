444c0d0ac40d345aab5c153010f6f92f3c02fdf6
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-28 17:40:29 -0400

    add def destroy which sets session user_id to nil and redirects to login.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,15 +8,18 @@ class SessionsController < ApplicationController
       user = Programmer.find_by_email(params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to projects_path, notice: 'Loged in Successfully'
+        redirect_to projects_path,
+            flash[:notice] = 'Logged in Successfully'
       else
         format.html  { render action: 'new', flash.now[:notice] =
-          'The email or password entered does not match our records.' }
+            'The email or password entered does not match our records.' }
       end
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to signin_path, notice: "Logged out Successfully"
   end
 
 end
