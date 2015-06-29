03a3b7c2a514854f800fc93fed19edf4c16bcd29
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-29 05:44:54 -0400

    add method that makes two time entries than checks that programmers have two time entries. and projects have two time entries.

--- test/models/time_entry_test.rb
+++ test/models/time_entry_test.rb
@@ -1,5 +1,10 @@
 require 'test_helper'
 
 class TimeEntryTest < ActiveSupport::TestCase
-
+  entry_one = TmeEntry.create(duration: 12, developer_id: 1,
+      project_id: 1 )
+  entry_two = TimeEntry.create(duration: 15, developer_id: 1,
+      project_id: 1 )
+  assert_equal 2, Developer.find_by_id(1).time_entries.count
+  assert_equal 2, Project.find_by_id(1).time_entries.count
 end
