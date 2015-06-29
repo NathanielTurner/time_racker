b4ae05dd5a9b34c09ece74aa53bebc972c5f6fad
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-06-29 05:52:42 -0400

    accidentally wrote last commits def for projects. changed to developer.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,3 +1,10 @@
 class Programmer < ActiveRecord::Base
   has_secure_password
+  has_many :time_entries
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    return "#{first} #{last}"
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,3 @@
 class Project < ActiveRecord::Base
-  has_many :time_entries
-
-  def full_name
-    first = self.first_name
-    last = self.last_name
-    return "#{first} #{last}"
-  end
+  
 end
